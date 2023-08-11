3821b64c5221a0fc7db605015691075b32209ee2
JP Tasto <jtasto@sandiego.edu>
2023-08-10 23:06:19 -0700

    Test: Created tests for PawnTest

--- src/test/java/edu/sandiego/comp305/sp23/PawnTest.java
+++ src/test/java/edu/sandiego/comp305/sp23/PawnTest.java
@@ -1,4 +1,32 @@
+package edu.sandiego.comp305.sp23;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class PawnTest {
-  
+
+class PawnTest{
+
+    @Test
+    void testEquals(){
+        Pawn pawn = new Pawn(PawnTypes.BLACK, -2);
+        Pawn otherPawn = new Pawn(PawnTypes.BLACK, -2);
+
+        assertEquals(pawn, otherPawn);
+    }
+
+    @Test
+    void testNotEqual(){
+        Pawn pawn = new Pawn(PawnTypes.BLACK, -2);
+        Pawn otherPawn = new Pawn(PawnTypes.WHITE, 1);
+
+        assertNotEquals(pawn, otherPawn);
+    }
+
+    @Test
+    void testCompareToNonPawn(){
+        Pawn pawn = new Pawn(PawnTypes.BLACK, -2);
+        Integer notAPawn = 1;
+
+        assertNotEquals(pawn, notAPawn);
+    }
 }
