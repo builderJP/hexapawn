6efff47a72642531b8826663e3424e44b361941b
JP Tasto <jtasto@sandiego.edu>
2023-08-16 17:01:01 -0700

    Test: Added initial tests to HexapawnBlackPerfectGameTree

--- src/test/java/edu/sandiego/comp305/sp23/HexapawnBlackPerfectGameTreeTest.java
+++ src/test/java/edu/sandiego/comp305/sp23/HexapawnBlackPerfectGameTreeTest.java
@@ -1,4 +1,36 @@
+package edu.sandiego.comp305.sp23;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class HexapawnBlackGameTreeTest {
-  
+
+class HexapawnBlackPerfectGameTreeTest{
+
+    private static HexapawnBlackPerfectGameTree blackAI;
+
+    @BeforeAll
+    public static void createBlackGameTree(){
+        blackAI = new HexapawnBlackPerfectGameTree();
+    }
+
+    @Test
+    void getBestMove(){
+        Pawn[][] whiteMovesLeftPawn = {
+                {new Pawn(PawnTypes.BLACK, -1), new Pawn(PawnTypes.BLACK, -2), new Pawn(PawnTypes.BLACK, -3)},
+                {new Pawn(PawnTypes.WHITE, 1), new Pawn(PawnTypes.BLANK_SPACE, 0), new Pawn(PawnTypes.BLANK_SPACE, 0)},
+                {new Pawn(PawnTypes.BLANK_SPACE, 0), new Pawn(PawnTypes.WHITE, 2), new Pawn(PawnTypes.WHITE, 3)} };
+
+        assertEquals("-2|aw", blackAI.getBestMove(whiteMovesLeftPawn));
+    }
+
+    @Test
+    void emptyStringForBoardsNotInGameTree(){
+        Pawn[][] invalidBoard = {
+                {new Pawn(PawnTypes.BLACK, -1), new Pawn(PawnTypes.BLACK, -2), new Pawn(PawnTypes.BLACK, -3)},
+                {new Pawn(PawnTypes.BLANK_SPACE, 0), new Pawn(PawnTypes.BLANK_SPACE, 0), new Pawn(PawnTypes.BLANK_SPACE, 0)},
+                {new Pawn(PawnTypes.WHITE, 1), new Pawn(PawnTypes.WHITE, 2), new Pawn(PawnTypes.WHITE, 3)} };
+
+        assertEquals("", blackAI.getBestMove(invalidBoard));
+    }
 }
