d874d3fa518931e79ee14abba913dafd15dca586
JP Tasto <jtasto@sandiego.edu>
2023-08-21 14:32:54 -0700

    Docs: HexapawnWhitePlayerPrompts & its test JavaDocs done, Class UML diagram updated

--- src/main/java/edu/sandiego/comp305/sp23/HexapawnWhitePlayerPrompts.java
+++ src/main/java/edu/sandiego/comp305/sp23/HexapawnWhitePlayerPrompts.java
@@ -1,13 +1,38 @@
 package edu.sandiego.comp305.sp23;
 
+/**
+ * This class provides all possible prompts for the white player in a Hexapawn game where the black player plays perfectly.
+ * The possible moves white can make are dependent on the current state of the game board and a list of these possible moves are returned in the String.
+ * @author JP Tasto
+ */
 public class HexapawnWhitePlayerPrompts{
 
     protected HexapawnBoard hexapawnBoard;
 
-    HexapawnWhitePlayerPrompts(){
+    /**
+     * Constructs a new instance of the HexapawnWhitePlayerPrompts class.
+     * Initializes the class with a new instance of the HexapawnBoard class.
+     */
+    public HexapawnWhitePlayerPrompts(){
         this.hexapawnBoard = new HexapawnBoard();
     }
 
+    /**
+     * Constructs a new instance of the HexapawnWhitePlayerPrompts class.
+     * Initializes the class with a preexisting instance of the HexapawnBoard class.
+     * @param hexapawnBoard The preexisting HexapawnBoard instance
+     */
+    public HexapawnWhitePlayerPrompts(HexapawnBoard hexapawnBoard){
+        this.hexapawnBoard = hexapawnBoard;
+    }
+
+    /**
+     * Generates prompts for the white player's possible moves based on the current state of the game board. The list of
+     * game board variations are the ones that exist if black plays perfectly. The prompts are returned as a formatted string.
+     * If the game board state isn't in the list of states that exist if black plays perfectly, an empty String is returned.
+     * @return A String with prompts for the white player's possible moves. Is empty if the game board state is not in the
+     *          list of game board states if black plays perfectly.
+     */
     public String getWhitePlayerPrompts(){
         Pawn[][] board = this.hexapawnBoard.getBoard();
 
--- src/test/java/edu/sandiego/comp305/sp23/HexapawnWhitePlayerPromptsTest.java
+++ src/test/java/edu/sandiego/comp305/sp23/HexapawnWhitePlayerPromptsTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test class for HexapawnWhitePlayerPromptsTest
+ * @author JP Tasto
+ */
 class HexapawnWhitePlayerPromptsTest{
 
     private static HexapawnWhitePlayerPrompts whitePlayer;
@@ -26,6 +30,9 @@ class HexapawnWhitePlayerPromptsTest{
         whitePlayer.hexapawnBoard.resetBoard();
     }
 
+    /**
+     * Tests whether the generated prompt matches the expected output for the initial game board configuration.
+     */
     @Test
     void whiteCorrectPrompts(){
         Pawn[][] board = whitePlayer.hexapawnBoard.getBoard();
@@ -36,6 +43,10 @@ class HexapawnWhitePlayerPromptsTest{
         assertEquals(expectedOutput, whitePlayer.getWhitePlayerPrompts());
     }
 
+    /**
+     * Tests whether an empty string is returned when the game board is not in the list of game board states if black plays perfectly.
+     * This specific example is for a game board state where it is black's turn to play.
+     */
     @Test
     void emptyStringForBoardsNotInGameTree(){
         Pawn[][] board = whitePlayer.hexapawnBoard.getBoard();
