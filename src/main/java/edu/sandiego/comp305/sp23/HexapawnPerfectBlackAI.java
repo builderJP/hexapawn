7547fb92113345617b30706e8d9c49fd93778cf3
JP Tasto <jtasto@sandiego.edu>
2023-08-21 14:16:54 -0700

    Refactor: HexapawnPerfectBlackAI has constructor w/ parameter, constructors public

--- src/main/java/edu/sandiego/comp305/sp23/HexapawnPerfectBlackAI.java
+++ src/main/java/edu/sandiego/comp305/sp23/HexapawnPerfectBlackAI.java
@@ -14,8 +14,17 @@ public class HexapawnPerfectBlackAI{
      * Constructs a new instance of the HexapawnPerfectBlackAI class.
      * Initializes the class with a new instance of the HexapawnBoard class.
      */
-    HexapawnPerfectBlackAI(){
+    public HexapawnPerfectBlackAI(){
         this.hexapawnBoard = new HexapawnBoard();
+    }
+
+    /**
+     * Constructs a new instance of the HexapawnPerfectBlackAI class.
+     * Initializes the class with a preexisting instance of the HexapawnBoard class.
+     * @param hexapawnBoard The preexisting HexapawnBoard instance
+     */
+    public HexapawnPerfectBlackAI(HexapawnBoard hexapawnBoard){
+        this.hexapawnBoard = hexapawnBoard;
     }
 
     /**
@@ -24,25 +33,27 @@ public class HexapawnPerfectBlackAI{
      * and executes the chosen move on the game board.
      */
     public void blackDoBestMove(){
+        Pawn[][] board = this.hexapawnBoard.getBoard();
+        
         switch(this.hexapawnBoard.toString()){
             //White moves leftmost pawn as first move
-            case "BBBW   WW" -> hexapawnBoard.attack(hexapawnBoard.getBoard()[0][1].getPawnID(), true);
+            case "BBBW   WW" -> hexapawnBoard.attack(board[0][1].getPawnID(), true);
             //Below are responses to the possible moves white can do after black responds to their initial move
-            case "B BW    W" -> hexapawnBoard.move(hexapawnBoard.getBoard()[0][2].getPawnID());
-            case "B BBW   W", "B BB W W " -> hexapawnBoard.move(hexapawnBoard.getBoard()[1][0].getPawnID());
+            case "B BW    W" -> hexapawnBoard.move(board[0][2].getPawnID());
+            case "B BBW   W", "B BB W W " -> hexapawnBoard.move(board[1][0].getPawnID());
 
             //White moves middle pawn as first move
-            case "BBB W W W" -> hexapawnBoard.attack(hexapawnBoard.getBoard()[0][2].getPawnID(), true);
+            case "BBB W W W" -> hexapawnBoard.attack(board[0][2].getPawnID(), true);
             //Below are responses to possible moves white can do after black responds to their initial move
-            case "BB WB   W", "BB  BWW  " -> hexapawnBoard.move(hexapawnBoard.getBoard()[1][1].getPawnID());
-            case "BB  W W  ", "BB  W   W" -> hexapawnBoard.move(hexapawnBoard.getBoard()[0][0].getPawnID());
-            case " B BWW   " -> hexapawnBoard.move(hexapawnBoard.getBoard()[1][0].getPawnID());
+            case "BB WB   W", "BB  BWW  " -> hexapawnBoard.move(board[1][1].getPawnID());
+            case "BB  W W  ", "BB  W   W" -> hexapawnBoard.move(board[0][0].getPawnID());
+            case " B BWW   " -> hexapawnBoard.move(board[1][0].getPawnID());
 
             //White moves rightmost pawn as first move
-            case "BBB  WWW " -> hexapawnBoard.attack(hexapawnBoard.getBoard()[0][1].getPawnID(), false);
+            case "BBB  WWW " -> hexapawnBoard.attack(board[0][1].getPawnID(), false);
             //Below are responses to possible moves white can do after black responds to their initial move
-            case "B B  WW  " -> hexapawnBoard.move(hexapawnBoard.getBoard()[0][0].getPawnID());
-            case "B BW B W ", "B B WBW  " -> hexapawnBoard.move(hexapawnBoard.getBoard()[1][2].getPawnID());
+            case "B B  WW  " -> hexapawnBoard.move(board[0][0].getPawnID());
+            case "B BW B W ", "B B WBW  " -> hexapawnBoard.move(board[1][2].getPawnID());
         }
     }
 }
